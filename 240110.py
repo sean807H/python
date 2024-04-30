b8b0bf3e53fe1de7e378b0e313ecc36acd3a31dd
김서연 <129020628+sean807H@users.noreply.github.com>
2024-04-30 15:07:30 +0900

    python_01
    파이썬 선행

---
+++ 240110.py
@@ -0,0 +1,21 @@
+# a = "Life is too short, You need Python"
+# w = a[0] #인덱싱
+# b = a[0:4]  #슬라이싱
+# print(b)
+# c = a.split()[0] #split 나누어지다
+# print(c)
+
+# a = "안녕하세요" #문자열
+# b = a[0]   #인덱싱
+# c = a[0:4] #슬라이싱
+# d = a.split()[0]  #스플릿
+# name = "김서연"
+# print(f"안녕하세요 저는 {name} 입니다")
+# a.count('v')    #문자개수세기
+# a.find/a.index  #위치알려주기
+
+# a.append    #리스트 요소 추가
+# a.insert(0,3)   #리스트 요소 삽입
+
+# for문과 많이 쓰이는 range(1,10) #리스트를 자동으로 만들어줌(튜플,문자열)
+# 초기화는 무조건!!
