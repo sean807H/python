bdfdb99de316a3062079deef4ebfe29389350216
김서연 <129020628+sean807H@users.noreply.github.com>
2024-05-02 14:08:19 +0900

    python_02
    파이썬 선행

---
+++ 240113.py
@@ -0,0 +1,35 @@
+# a = ['Life', 'is', 'too', 'short']
+
+# print(a.index('Life'))
+# x = a
+# print(type(x))
+# print(x[1])
+
+# a = [1, 2, 3]
+# a.insert(2,4)   #리스트에 요소 삽입
+# print(a.index(1))   #몇번째에 있는지 인덱스 반환
+
+# a.append() #리스트 요소 추가
+# a.extend() #리스트 확장
+
+#튜플 삭제&변경X
+
+# a = {'김연아' : '피겨스케이팅', '김서연':'프로그래머'}
+# print(a['김서연'])
+# print(a.keys()) #keys 값 리턴
+# b = list(a.keys())
+
+# print(b[0])
+# print(b[1])
+
+# print(a.values())   #values 값 리턴
+# c = list(a.values())
+# print(c[1])
+
+# print(a.items())    #keys&values 값 리턴
+# d = list(a.items())
+# print(d[1])
+
+# print(a['김서연'])  #반환값 없음
+# print(a.get('김서연')) #반환값 none
+
