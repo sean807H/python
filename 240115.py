91997b6998d6a2c008afd7f29d25dd1255c6d34e
김서연 <129020628+sean807H@users.noreply.github.com>
2024-05-03 21:42:15 +0900

    python_03
    파이썬 선행

---
+++ 240115.py
@@ -0,0 +1,38 @@
+# a = "Life is too short, You need Python"
+# print(a[19:22])
+# print(a.split()[4])
+# print(a[12:17])
+
+# b = a.split()[2][:2]+'is'
+# c = b[:2]
+# d = c+" is"   
+# print(d)
+# a= [1,2,3,4]
+# b=[num * 3 for num in a]
+# b=[]
+# for num in a:
+#     b.append(num * 3)
+# print(b)
+
+# money = 2000
+# if money >=3000:
+#     print("택시를")
+#     print("타고 가세요")
+# else:
+#     print("싫은데용")
+
+# pocket = ["안녕","네","DK"]
+# if "DK" in pocket:
+#     print("안녕하세요")
+# elif "네" not in pocket:
+#     print("누구세여야")
+# else:
+#     pass
+
+# if score >= 60:
+#     message = "success"
+# else:
+#     message = "failure"
+
+# message = "success" if score >= 60 else message = "failure"
+
