cc02c07595e1b77601305d90da909fae297a3abb
김서연 <129020628+sean807H@users.noreply.github.com>
2024-08-01 00:32:01 +0900

    python

---
+++ PY4E/CRVAexam.py
@@ -0,0 +1,21 @@
+#상수
+print(123)  #상수는 값이 변하지 않음
+print(98.6)
+print('hello world')
+
+#예약어
+
+#변수
+x=88
+print(x)    #88출력
+y=14
+x=18
+print(x)    #18출력
+
+#규칙
+#문자 또는 underscore(_)시작. 숫자X
+
+#할당문
+x=0.6
+x=3.9*x*(1-x)   #3.9*0.6*0.4
+print(x)
