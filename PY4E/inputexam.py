5be597be323463f85e06b979766b1675f44dc596
김서연 <129020628+sean807H@users.noreply.github.com>
2024-08-27 19:43:47 +0900

    python

---
+++ PY4E/inputexam.py
@@ -0,0 +1,9 @@
+#2.2
+nzt=input("Enter your name: ")
+print("Hello",nzt)
+
+#2.3
+hours=float(input("일한 시간을 입력하세요: "))
+rate_per_hour=float(input("시간당 급여를 입력하세요: "))
+total_wage=hours*rate_per_hour
+print("임금 총액은 ",total_wage," 입니다.")
